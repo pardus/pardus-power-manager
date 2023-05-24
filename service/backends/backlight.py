d5fe3bc41b5c73f45e34fb4334ee1bb3640b1c11
aliriza.keskin <aliriza.keskin@pardus.org.tr>
2023-05-24 14:49:36 +0300

    backlight backend

--- service/backends/backlight.py
+++ service/backends/backlight.py
@@ -6,6 +6,8 @@ class backlight_devices:
         self.max_brightness = 0
 
 def get_max_brightness(device_name):
+    if not os.path.exists("/sys/class/backlight/{}/max_brightness".format(device_name)):
+        return 0
     fd = readfile("/sys/class/backlight/{}/max_brightness".format(device_name))
     return int(fd)
 
@@ -13,10 +15,14 @@ def get_devices():
     return os.listdir("/sys/class/backlight/")
 
 def get_brightness(device_name):
+    if not os.path.exists("/sys/class/backlight/{}/brightness".format(device_name)):
+        return 0
     fd = readfile("/sys/class/backlight/{}/brightness".format(device_name))
     return int(fd)
 
 def set_brightness(device_name,value):
+    if not os.path.exists("/sys/class/backlight/{}/brightness".format(device_name)):
+        return
     fd = open("/sys/class/backlight/{}/brightness".format(device_name),"w")
     fd.write(str(int(value)))
     fd.flush()
--- service/service.py
+++ service/service.py
@@ -1,13 +1,17 @@
 from util import send_client
-import power
-import backlight
+import backends.power as power
+import backends.backlight as backlight
 def main(data):
     print(data)
     if "new-mode" in data:
         power.set_mode(data["new-mode"])
+    if "new-backlight" in data:
+        for dev in data[new-backlight]:
+             backlight.set_brightness(dev, data[new-backlight][dev])
     udata = {}
     udata["current-mode"] = power.get_mode()
     udata["current-backlight"] = {}
-    for dev in backlightd.get_devices():
+    for dev in backlight.get_devices():
         udata["current-backlight"][dev] = backlight.get_brightness(dev)
+    print(udata)
     send_client(udata)
