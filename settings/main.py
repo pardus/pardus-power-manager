39439e8b79da2fb0bfaa8c0564351f3cb21868b6
aliriza.keskin <aliriza.keskin@pardus.org.tr>
2023-07-07 08:24:38 +0000

    settings variable fill on start

--- settings/main.py
+++ settings/main.py
@@ -10,6 +10,14 @@ from gi.repository import GLib, Gtk
 sys.path.insert(0, os.path.dirname( os.path.realpath(__file__) )+"/../common")
 from common import *
 
+def fint(ctx):
+    ret = ""
+    for c in ctx:
+        if c in "0123456789":
+            ret += c
+    return int(ret)
+
+
 class MainWindow:
     def __init__(self):
         self.builder = Gtk.Builder()
@@ -18,6 +26,7 @@ class MainWindow:
         self.connect_signals()
         self.combobox_init()
         self.spinbutton_init()
+        self.value_init()
 
     def combobox_init(self):
         store = Gtk.ListStore(str, str)
@@ -39,6 +48,17 @@ class MainWindow:
         self.o("ui_spinbutton_performance").set_increments(1,1)
         self.o("ui_spinbutton_powersave").set_digits(0)
         self.o("ui_spinbutton_performance").set_digits(0)
+
+
+    def value_init(self):
+        self.o("ui_switch_service").set_state(get("enabed",True,"service"))
+        self.o("ui_switch_gpu").set_state(get("disable-3d-controller",False,"service"))
+        self.o("ui_spinbutton_powersave").set_value(fint(get("backlight-powersave","%50","modes")))
+        self.o("ui_spinbutton_performance").set_value(fint(get("backlight-performance","%100","modes")))
+        l = ["performance", "powersave", "ignore"]
+        self.o("ui_combobox_acmode").set_active(l.index(get("ac-mode","performance","modes")))
+        self.o("ui_combobox_batmode").set_active(l.index(get("bat-mode","powersave","modes")))
+
 
     def connect_signals(self):
         self.window.connect("destroy",Gtk.main_quit)
