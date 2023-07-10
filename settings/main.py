0d10141c8071a7eefdf2b9d67a0cdd3f6894ab71
aliriza <aliriza.keskin@pardus.org.tr>
2023-07-10 15:23:09 +0000

    save button removed from settings and reboot dialog removed

--- settings/main.py
+++ settings/main.py
@@ -10,8 +10,6 @@ from gi.repository import GLib, Gtk
 sys.path.insert(0, os.path.dirname( os.path.realpath(__file__) )+"/../common")
 from common import *
 
-from reboot_dialog import RebootDialog
-
 def fint(ctx):
     ret = ""
     for c in ctx:
@@ -25,10 +23,10 @@ class MainWindow:
         self.builder = Gtk.Builder()
         self.builder.add_from_file(os.path.dirname(os.path.abspath(__file__)) + "/../data/MainWindow.ui")
         self.window = self.builder.get_object("ui_window_main")
-        self.connect_signals()
         self.combobox_init()
         self.spinbutton_init()
         self.value_init()
+        self.widget_changes_event_init()
 
     def combobox_init(self):
         store = Gtk.ListStore(str, str)
@@ -61,16 +59,18 @@ class MainWindow:
         self.o("ui_combobox_acmode").set_active(l.index(get("ac-mode","performance","modes")))
         self.o("ui_combobox_batmode").set_active(l.index(get("bat-mode","powersave","modes")))
 
-
-    def connect_signals(self):
-        self.window.connect("destroy",Gtk.main_quit)
-        self.o("ui_button_cancel").connect("clicked",Gtk.main_quit)
-        self.o("ui_button_save").connect("clicked",self.save_settings)
+    def widget_changes_event_init(self):
+        self.o("ui_switch_service").connect("state-set",self.save_settings)
+        self.o("ui_switch_gpu").connect("state-set",self.save_settings)
+        self.o("ui_combobox_acmode").connect("changed",self.save_settings)
+        self.o("ui_combobox_batmode").connect("changed",self.save_settings)
+        self.o("ui_spinbutton_powersave").connect("value-changed",self.save_settings)
+        self.o("ui_spinbutton_performance").connect("value-changed",self.save_settings)
 
     def o(self,name):
         return self.builder.get_object(name)
 
-    def save_settings(self, widget):
+    def save_settings(self, a=None, b=None):
         data = {}
         # service
         data["service"] = {}
@@ -81,9 +81,11 @@ class MainWindow:
         ac_w = self.o("ui_combobox_acmode")
         bat_w = self.o("ui_combobox_batmode")
         t = ac_w.get_active_iter()
-        data["modes"]["ac-mode"] = ac_w.get_model()[t][1]
+        if t:
+            data["modes"]["ac-mode"] = ac_w.get_model()[t][1]
         t = bat_w.get_active_iter()
-        data["modes"]["bat-mode"] = bat_w.get_model()[t][1]
+        if t:
+            data["modes"]["bat-mode"] = bat_w.get_model()[t][1]
         # backlight
         data["modes"]["backlight-powersave"] = "%"+str(int(self.o("ui_spinbutton_powersave").get_value()))
         data["modes"]["backlight-performance"] = "%"+str(int(self.o("ui_spinbutton_performance").get_value()))
@@ -96,10 +98,8 @@ class MainWindow:
             for var in data[section]:
                 ctx += str(var) + "=" + str(data[section][var]) +"\n"
             ctx += "\n"
-        writefile("/etc/pardus/ppm.conf.d/99-ppm-settings,conf",ctx)
         print(ctx)
-        self.window.hide()
-        RebootDialog()
+        writefile("/etc/pardus/ppm.conf.d/99-ppm-settings,conf",ctx)
 
 if __name__ == "__main__":
     if os.getuid() != 0 and "--test" not in sys.argv:
