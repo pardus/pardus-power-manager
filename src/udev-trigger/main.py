63d9b4bd25235d493547e8169afd1a7d6c96b1d5
sulincix <parduscix@yandex.ru>
2024-05-05 22:55:24 +0300

    ignore if last event is same with now

--- src/udev-trigger/main.py
+++ src/udev-trigger/main.py
@@ -9,9 +9,26 @@ data["pid"]="1"
 data["ENV"]=os.environ.copy()
 
 if os.path.exists("/run/ppm"):
+    ac_now = get_ac_online()
+
+    # get last status
+    ac_last = ""
+    if os.path.isfile("/run/ppm.last"):
+        with open("/run/ppm.last","r") as f:
+            ac_last = f.read()
+
+    # write last status
+    with open("/run/ppm.last","w") as f:
+        f.write(str(ac_now))
+
+    # ignore if last same with now
+    if str(ac_now) == str(ac_last):
+        exit(0)
+
     new_mode = get("ac-mode","performance","modes")
-    if not get_ac_online():
+    if not ac_now:
         new_mode = get("bat-mode","powersave","modes")
+
     data["new-mode"] = new_mode
     with open("/run/ppm", "w") as f:
         f.write(json.dumps(data))
