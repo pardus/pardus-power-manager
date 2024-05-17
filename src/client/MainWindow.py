1f26aafc36c04a2dd642973031cb958594f9ff09
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2024-05-17 11:24:13 +0300

    fixes about wmclass and meson

--- src/client/MainWindow.py
+++ src/client/MainWindow.py
@@ -52,7 +52,7 @@ class MainWindow:
             "pardus-power-manager", "pardus-pm-performance-symbolic", appindicator.IndicatorCategory.APPLICATION_STATUS)
         self.indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
         self.indicator.set_icon("pardus-pm-powersave-symbolic")
-        self.set_wmclass("pardus-power-manager")
+        self.window.set_wmclass("pardus-power-manager", "pardus-power-manager")
 
 
         Notify.init("Pardus Power Manager")
